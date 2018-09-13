4769147dbfce468569149c60869d532b127cd350
Your Name <you@example.com>
2018-09-13 10:07:58 +0100

    create place order modal

--- UI/js/default.js
+++ UI/js/default.js
@@ -10,3 +10,34 @@ if (navbarSlider) {
 }
 
 
+// content and idea gotten from w3schools.com
+// Get the modal
+var modal = document.getElementById('myModal');
+
+// Get the button that opens the modal
+var btn = document.getElementsByClassName("order-btn");
+
+// iterate over all the elements
+for (var i= btn.length; i--;) {
+  btn[i].addEventListener('click', setModal, false);
+}
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+ function setModal() {
+    modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
