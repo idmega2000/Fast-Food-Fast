aef6b411536b850e4d9e92270f9e87e9ed9cc0f8
Your Name <you@example.com>
2018-09-21 11:35:07 +0100

    implement order cart page ui

--- js/default.js
+++ js/default.js
@@ -1,27 +1,40 @@
-const scrollButton = document.getElementById("arrowP");
+const scrollButton = document.getElementById('arrowP');
 const navbarSlider = document.getElementById('header-sm-sc');
-const rejectBtn = document.getElementsByClassName("reject-btn");
+const placeOrderBtn = document.getElementById('placeOrderBtn');
+const compOrdDiv = document.getElementById('compOrdDiv');
+const canBookBtn = document.getElementById('canBookBtn');
+
+
+if (placeOrderBtn) {
+  placeOrderBtn.onclick = () => {
+    compOrdDiv.style.display = 'block';
+    document.body.scrollTop = 550;
+    document.documentElement.scrollTop = 550;
+  };
+}
+
+if (canBookBtn) {
+  canBookBtn.onclick = () => {
+    compOrdDiv.style.display = 'none';
+  };
+}
 
 const drawNav = () => {
-    const navDrawer = document.getElementById('slide-nav');
-    navDrawer.classList.toggle("nav-slide-toggle");
+  const navDrawer = document.getElementById('slide-nav');
+  navDrawer.classList.toggle('nav-slide-toggle');
 };
 
 
-const setModal = () => {
-    modal.style.display = "block";
-} 
-
 if (navbarSlider) {
-    navbarSlider.addEventListener('click', drawNav);
+  navbarSlider.addEventListener('click', drawNav);
 }
 
 
 const moveDownwards = () => {
-    document.body.scrollTop = 600;
-    document.documentElement.scrollTop = 600;
-}
+  document.body.scrollTop = 600;
+  document.documentElement.scrollTop = 600;
+};
 
 if (scrollButton) {
-    scrollButton.onclick = moveDownwards;
+  scrollButton.onclick = moveDownwards;
 }
---
+++ js/modal.js
@@ -0,0 +1,71 @@
+// content and idea gotten from w3schools.com
+
+
+const modal = document.getElementById('myModal');
+const modalDel = document.getElementById('myModalDel');
+
+const delAOrder = document.getElementById('delAOrder');
+const editBtn = document.getElementsByClassName('edit-btn');
+const rejectBtn = document.getElementsByClassName('reject-btn');
+const modalBtn = document.getElementsByClassName('modal-btn');
+const closeModal = document.getElementsByClassName('close')[0];
+const closeModalButton = document.getElementById('closeModal');
+const closeModalDbtn = document.getElementById('closeModaldel');
+
+const setModal = () => {
+  modal.style.display = 'block';
+};
+
+
+for (let i = 0; i < modalBtn.length; i += 1) {
+  modalBtn[i].addEventListener('click', setModal, false);
+}
+
+
+const setDelModalMsg = () => {
+  if (modal) {
+    modal.style.display = 'none';
+  }
+  modalDel.style.display = 'block';
+};
+
+if (delAOrder) {
+  delAOrder.onclick = () => {
+    setDelModalMsg();
+  };
+}
+
+
+if (closeModalButton) {
+  closeModalButton.onclick = () => {
+    modal.style.display = 'none';
+  };
+}
+if (closeModalDbtn) {
+  closeModalDbtn.onclick = () => {
+    modalDel.style.display = 'none';
+  };
+}
+
+
+if (closeModal) {
+  closeModal.onclick = () => {
+    modal.style.display = 'none';
+  };
+}
+
+window.onclick = (event) => {
+  if (event.target === modal) {
+    modal.style.display = 'none';
+  }
+};
+
+const setRedirect = (input) => {
+  window.location.href = input;
+};
+
+/*
+for (let i = 0; i < editBtn.length; i++) {
+    editBtn[i].onclick = setRedirect('edit-fastfood.html');
+}
+*/
