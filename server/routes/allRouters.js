5de1c355b265b7b45736cc8d51d85caa86bf91ee
Your Name <you@example.com>
2018-10-03 08:29:10 +0100

    setup swagger api-docs

--- server/routes/allRouters.js
+++ server/routes/allRouters.js
@@ -1,12 +1,17 @@
 import express from 'express';
+import swaggerUi from 'swagger-ui-express';
 import authRouter from './authRouter';
 import ordersRouter from './ordersRouter';
 import menuRouter from './menuRouter';
 import usersRouter from './usersRouter';
 import statusRouter from './statusRouter';
+import swaggerDocument from '../swagger.json';
 
 const allRouters = express.Router();
-
+allRouters.use('/api-docs',
+  swaggerUi.serve,
+  swaggerUi
+    .setup(swaggerDocument));
 allRouters
   .use('/api/v1/menu',
     menuRouter);
